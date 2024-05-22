5fe08cc8e73d1052c1a46d4eddff3997cc201f97
tiago <tiago.zdo@gmail.com>
2024-05-22 13:03:16 -0300

    refactor: Completely remove bootstrap code

--- src/components/Newsitem.jsx
+++ src/components/Newsitem.jsx
@@ -1,13 +1,14 @@
 import image from "../assets/news.jpg";
+import styles from "./Newsitem.module.css";
 
 export const Newsitem = ({ title, description, src, url }) => {
     return (
-        <div className="news-item">
-            <img className="news-image" src={src ? src : image}  alt="..." />
-            <div className="news-body">
-                <h5 className="news-title">{title.slice(0, 50)}</h5>
+        <div className={styles.newsItem}>
+            <img className={styles.newsImage} src={src ? src : image} alt="..." />
+            <div className={styles.newsBody}>
+                <h5 className={styles.newsTitle}>{title.slice(0, 50)}</h5>
                 <p>{description ? description.slice(0, 90) : "Texto..."}</p>
-                <a href={url} className="news-btn">Read More</a>
+                <a href={url} className={styles.newsBtn}>Read More</a>
             </div>
         </div>
     )
